68ac305b1fea25bb2be737edf9e9dc1a4e7f289b
Dylan Southard <kamisama42@gmail.com>
2018-04-21 17:01:44 +0700

    Initial Commit

---
+++ MoviePicker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MoviePicker
+//
+//  Created by Dylan Southard on 2018/04/21.
+//  Copyright © 2018 Dylan Southard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
